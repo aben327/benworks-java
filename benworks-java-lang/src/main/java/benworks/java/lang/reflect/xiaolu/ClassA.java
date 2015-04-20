827e36d87fc16bae96df48b8269283331f3015da
Ben <Ben@gmail.com>
2015-04-20 16:50:16 +0800

    动态代理示例

---
+++ benworks-java-lang/src/main/java/benworks/java/lang/reflect/xiaolu/ClassA.java
@@ -0,0 +1,11 @@
+package benworks.java.lang.reflect.xiaolu;
+
+// 具体类A
+class ClassA implements AbstractClass {
+
+	@Override
+	public void show() {
+		System.out.println("我是A类！");
+	}
+
+}
---
+++ benworks-java-lang/src/main/java/benworks/java/lang/reflect/xiaolu/ClassB.java
@@ -0,0 +1,11 @@
+package benworks.java.lang.reflect.xiaolu;
+
+// 具体类A
+class ClassB implements AbstractClass {
+
+	@Override
+	public void show() {
+		System.out.println("我是B类！");
+	}
+
+}

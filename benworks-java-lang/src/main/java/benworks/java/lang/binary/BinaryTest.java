01a3bf4789baa54a904f3f897b3bf7afb075f61b
aben328 <aben328@gmail.com>
2016-04-26 14:54:51 +0800

    StrBinaryTurn

---
+++ benworks-java-lang/src/main/java/benworks/java/lang/binary/BinaryTest.java
@@ -0,0 +1,42 @@
+package benworks.java.lang.binary;
+
+/**
+ * 常见进制转换
+ * @author Ben
+ */
+public class BinaryTest {
+
+	public static void main(String[] args) {
+
+		int i = 17;
+
+		// 十进制转成二进制
+		String dec2Bin = Integer.toBinaryString(i);
+		System.out.println("十进制转成二进制:" + i + "->" + dec2Bin);
+
+		// 十进制转成八进制
+		String dec2Otc = Integer.toOctalString(i);
+		System.out.println("十进制转成八进制:" + i + "->" + dec2Otc);
+
+		// 十进制转成十六进制
+		String dec2Hex = Integer.toHexString(i);
+		System.out.println("十进制转成十六进制:" + i + "->" + dec2Hex);
+
+		// 十六进制转成十进制
+		String hex = "FF";
+		String hex2Dec = Integer.valueOf(hex, 16).toString();
+		System.out.println("十六进制转成十进制:" + hex + "->" + hex2Dec);
+
+		// 八进制转成十进制
+		String otc = "144";
+		String otc2Dec = Integer.valueOf(otc, 8).toString();
+		System.out.println("八进制转成十进制:" + otc + "->" + otc2Dec);
+
+		// 二进制转十进制
+		String bin = "0101";
+		String bin2Dec = Integer.valueOf(bin, 2).toString();
+		System.out.println("二进制转成十进制:" + bin + "->" + bin2Dec);
+
+	}
+
+}
